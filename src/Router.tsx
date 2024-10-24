4912506b5308315ccf84d3cf31eabd31589baaed
PNS212624\Hansol <yrlee00@hansol.com>
2024-10-24 17:20:24 +0900

    10/24 데이터 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
+import App from "./App";
 
 const router = createBrowserRouter([
     {
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,117 @@
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
+import styled from "styled-components";
+import {useEffect, useState} from "react";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+
+const Loader = styled.span`
+    text-align: center;
+    color: white;
+`;
+
+
+interface RouteState {
+    state: {
+        name:string;
+    }
+}
+
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        };
+    };
+}
 
 function Coin() {
     const { coinId } = useParams();
+    const [loading, setLoading] = useState(true);
+    const {state} = useLocation() as RouteState;
+    const [info, setInfo] = useState<InfoData>();
+    const [price, setPriceInfo] = useState<PriceData>();
 
-    return <h1>Coin : {coinId}</h1>
+    useEffect(() => {
+        (async () => {
+            const infoData = (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);  // todo 오류 해결하기
+            setPriceInfo(priceData);
+        })();
+    }, []);
+
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {
+                loading ? <Loader>Loading...</Loader> : null
+            }
+        </Container>
+
+    )
 }
 export default Coin;
